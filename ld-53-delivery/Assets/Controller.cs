8de2c92aeecb95a23b93bb95c5a66bc7cfce3b5c
Lex <lex.melotti@gmail.com>
2023-04-29 08:55:43 -0300

    Adapting to the new Input System

--- ld-53-delivery/Assets/Controller.cs
+++ ld-53-delivery/Assets/Controller.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
@@ -6,24 +7,38 @@ public class Controller : MonoBehaviour
 	public Transform RightMotor;
 	public float MotorForce;
 
+	private GameActions _gameActions;
+
+	private InputAction _leftThrottleAction;
+	private InputAction _rightThrottleAction;
+
 	private Rigidbody2D _myRigidBody;
 
-
-	private void Start()
+	private void Awake()
 	{
 		_myRigidBody = GetComponent<Rigidbody2D>();
+
+		_gameActions = new GameActions();
+
+		_leftThrottleAction = _gameActions.PlayerMovement.LeftThrottle;
+		_rightThrottleAction = _gameActions.PlayerMovement.RightThrottle;
+	}
+
+	private void OnEnable()
+	{
+		_leftThrottleAction.Enable();
+		_rightThrottleAction.Enable();
+	}
+
+	private void OnDisable()
+	{
+		_leftThrottleAction.Disable();
+		_rightThrottleAction.Disable();
 	}
 
 	private void FixedUpdate()
 	{
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			_myRigidBody.AddForceAtPosition(transform.up * MotorForce, LeftMotor.position, ForceMode2D.Force);
-		}
-
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			_myRigidBody.AddForceAtPosition(transform.up * MotorForce, RightMotor.position, ForceMode2D.Force);
-		}
+		_myRigidBody.AddForceAtPosition(_leftThrottleAction.ReadValue<float>() * transform.up * MotorForce, LeftMotor.position, ForceMode2D.Force);
+		_myRigidBody.AddForceAtPosition(_rightThrottleAction.ReadValue<float>() * transform.up * MotorForce, RightMotor.position, ForceMode2D.Force);
 	}
 }
